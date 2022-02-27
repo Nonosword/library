e004d843fadc4a71fabd5b736ca60ef770823ac3
Nonosword <37948612+Nonosword@users.noreply.github.com>
2022-02-27 13:34:43 +0800

    Update 2nd_startup.sh

--- r4s/2nd_startup.sh
+++ r4s/2nd_startup.sh
@@ -1,31 +1,34 @@
-rm -f *.deb
-
-echo "ubuntu ALL=(ALL:ALL) ALL">>/etc/sudoers
-echo "ubuntu ALL=(ALL) NOPASSWD:ALL">>/etc/sudoers
-
-su ubuntu <<EOF
-cd
+su ubuntu
 git clone https://github.com/coolsnowwolf/lede /home/ubuntu/lede
-
 cd /home/ubuntu/lede
+
 wget https://raw.githubusercontent.com/Nonosword/library/main/r4s/extra_feeds
 cat extra_feeds >> feeds.conf.default
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+# wget https://raw.githubusercontent.com/Nonosword/library/main/r4s/def_config
+# cat def_config > .config
+wget https://raw.githubusercontent.com/Nonosword/library/main/r4s/r4s_load.seed
+cat r4s_load.seed > .config
 wget https://raw.githubusercontent.com/Nonosword/library/main/r4s/patches.sh
 chmod +x ./patches.sh
 ./patches.sh
 
-wget https://raw.githubusercontent.com/Nonosword/library/main/r4s/def_config
-# wget https://raw.githubusercontent.com/Nonosword/library/main/r4s/extra_config
-# cat extra_config >> def_config
-cat def_config > .config
+sed -i 's/^[ \t]*//g' ./.config
+make defconfig
 
-# make -j8 download V=s
-# make -j1 V=s
+make download -j8 || make download -j1 V=s
+rm -rf $(find ./dl/ -size -1024c)
+df -h
 
-EOF
+make -j$(nproc) || make -j1 V=s
+echo "======================="
+echo "Space usage:"
+echo "======================="
+df -h
+echo "======================="
+du -h ./ --max-depth=1
+du -h /mnt/openwrt/ --max-depth=1 || true
 
 # simplefileserver
