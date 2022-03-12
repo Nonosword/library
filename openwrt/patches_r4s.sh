a662cb6ee8cb9d3a6e0555390a2f0bf66a002ba6
Nonosword <37948612+Nonosword@users.noreply.github.com>
2022-03-12 12:59:23 +0800

    Update patches_r4s.sh

--- openwrt/patches_r4s.sh
+++ openwrt/patches_r4s.sh
@@ -22,3 +22,6 @@ sed -i 's/192.168.1.1/192.168.2.1/' package/base-files/files/bin/config_generate
 # luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
 echo "CONFIG_PACKAGE_luci-app-jd-dailybonus=y" >> .config
+
+# fix adguardhome luci
+sed -i 's/AdGuardHome/adguardhome/' package/feeds/small/luci-app-adguardhome/Makefile
