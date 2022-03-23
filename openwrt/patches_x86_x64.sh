4787ae943bf6517455697ebd7c235d3eec270742
Nonosword <37948612+Nonosword@users.noreply.github.com>
2022-03-23 15:29:47 +0800

    Update patches_x86_x64.sh

--- openwrt/patches_x86_x64.sh
+++ openwrt/patches_x86_x64.sh
@@ -2,8 +2,8 @@ sed -i 's/192.168.1.1/192.168.100.1/' package/base-files/files/bin/config_genera
 sed -i 's/Openwrt/Openwrt_x64/' package/base-files/files/bin/config_generate
 
 # luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
-echo "CONFIG_PACKAGE_luci-app-jd-dailybonus=y" >> .config
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
+# echo "CONFIG_PACKAGE_luci-app-jd-dailybonus=y" >> .config
 
 # fix small8 adguardhome luci
 # rm -rf feeds/small8/adguardhome
