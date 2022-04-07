790d4078b906880cbf05c341557aeaaa729b8fa0
Nonosword <37948612+Nonosword@users.noreply.github.com>
2022-04-07 22:08:22 +0800

    Update patches_x86_x64.sh

--- openwrt/patches_x86_x64.sh
+++ openwrt/patches_x86_x64.sh
@@ -1,5 +1,3 @@
-sed -i 's/192.168.1.1/192.168.100.1/' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/Openwrt_x64/' package/base-files/files/bin/config_generate
 
 # luci-app-jd-dailybonus
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
