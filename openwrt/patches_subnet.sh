8469b4fa3968b8f06213bf7aed8d67df80d5164c
Nonosword <37948612+Nonosword@users.noreply.github.com>
2022-04-08 22:49:11 +0800

    Update patches_subnet.sh

--- openwrt/patches_subnet.sh
+++ openwrt/patches_subnet.sh
@@ -1,2 +1,4 @@
-sed -i 's/192.168.1.1/192.168.99.1/' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/R4S_Subnet/' package/base-files/files/bin/config_generate
+sed -i 's|192.168.1.1|192.168.99.1|' package/base-files/files/bin/config_generate
+sed -i 's|Openwrt|R4S_Subnet|' package/base-files/files/bin/config_generate
+sed -i 's|OpenWrt|R4S_Subnet|' package/base-files/files/bin/config_generate
+sed -i 's|log_size='64'|log_size='6400'|' package/base-files/files/bin/config_generate
