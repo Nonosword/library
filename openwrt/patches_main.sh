ccac056833a6c7805668d9f659a4740385f605c2
Nonosword <37948612+Nonosword@users.noreply.github.com>
2022-04-28 18:21:54 +0800

    Update patches_main.sh

--- openwrt/patches_main.sh
+++ openwrt/patches_main.sh
@@ -1,4 +1,4 @@
 sed -i 's|192.168.1.1|192.168.100.1|' package/base-files/files/bin/config_generate
-sed -i 's|Openwrt|R4S_Mainnet|' package/base-files/files/bin/config_generate
-sed -i 's|OpenWrt|R4S_Mainnet|' package/base-files/files/bin/config_generate
+sed -i 's|Openwrt|Devvvvvice_Mainnet|' package/base-files/files/bin/config_generate
+sed -i 's|OpenWrt|Devvvvvice_Mainnet|' package/base-files/files/bin/config_generate
 sed -i "s|log_size='64'|log_size='6400'|" package/base-files/files/bin/config_generate
