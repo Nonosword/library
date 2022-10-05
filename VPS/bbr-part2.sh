173a59d6fb0a2e60b881a5bc343ef1e483a82174
Nonosword <37948612+Nonosword@users.noreply.github.com>
2022-10-05 13:08:48 +0800

    Add files via upload

---
+++ VPS/bbr-part2.sh
@@ -0,0 +1,13 @@
+echo "net.ipv4.ip_forward=0" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=0" >> /etc/sysctl.conf
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sudo sysctl -p
+# check
+sudo sysctl net.ipv4.tcp_available_congestion_control
+sudo sysctl -n net.ipv4.tcp_congestion_control
+lsmod | grep bbr
+
+sed -e '/bbr-part2/d' /var/spool/cron/root
+service crond restart
+reboot
