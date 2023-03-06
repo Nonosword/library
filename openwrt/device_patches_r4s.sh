c1bcaa9f769a4fd91c1c48adf378e4852abd745f
Nonosword <37948612+Nonosword@users.noreply.github.com>
2023-03-06 22:48:13 +0800

    Update device_patches_r4s.sh

--- openwrt/device_patches_r4s.sh
+++ openwrt/device_patches_r4s.sh
@@ -8,5 +8,5 @@ echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
 echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
 
 # luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
-echo "CONFIG_PACKAGE_luci-app-jd-dailybonus=y" >> .config
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
+# echo "CONFIG_PACKAGE_luci-app-jd-dailybonus=y" >> .config
