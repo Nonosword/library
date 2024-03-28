2a4682f7c4b361d07f14b1267c388022a758c98a
Nonosword <37948612+Nonosword@users.noreply.github.com>
2024-03-28 17:03:43 +0800

    Update patches_generic.sh

--- openwrt/patches_generic.sh
+++ openwrt/patches_generic.sh
@@ -1,2 +1,2 @@
 sed -i 's|192.168.1.1|192.168.2.1|' package/base-files/files/bin/config_generate
-sed -i "s|log_size='64'|log_size='6400'|" package/base-files/files/bin/config_generate
+sed -i "s|log_size='64'|log_size='1024'|" package/base-files/files/bin/config_generate
