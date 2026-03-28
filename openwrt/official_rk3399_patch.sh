b78c2c4ac764f739753dd1a1ce2aded7291ada73
Nonosword <37948612+Nonosword@users.noreply.github.com>
2026-03-29 00:07:44 +0800

    Update official_rk3399_patch.sh

--- openwrt/official_rk3399_patch.sh
+++ openwrt/official_rk3399_patch.sh
@@ -1,1 +1,1 @@
-sed -i.bak 's/^  ifeq ($(HOST_ARCH),aarch64)$/  ifneq ($(filter aarch64 arm64,$(HOST_ARCH)),)/' package/boot/arm-trusted-firmware-rockchip/Makefile
+perl -pi.bak -e 's/^  ifeq \(\$\(HOST_ARCH\),aarch64\)$/  ifneq \(\$\(filter aarch64 arm64,\$\(HOST_ARCH\)\),\)/' package/boot/arm-trusted-firmware-rockchip/Makefile
