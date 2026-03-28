77082967f583401abbda7fe419f06afabd6e5ea7
Nonosword <37948612+Nonosword@users.noreply.github.com>
2026-03-29 00:08:39 +0800

    Update device_patches_r4s.sh

--- openwrt/device_patches_r4s.sh
+++ openwrt/device_patches_r4s.sh
@@ -1,11 +1,17 @@
-sed -i 's|Devvvvvice|R4S|' package/base-files/files/bin/config_generate
+if sed --version >/dev/null 2>&1; then
+  sed -i 's|Devvvvvice|R4S|' package/base-files/files/bin/config_generate
+else
+  sed -i '' 's|Devvvvvice|R4S|' package/base-files/files/bin/config_generate
+fi
 
-# change the voltage value for over-clock stablization
-config_file_cpufreq=`find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq'`
-truncate -s-1 $config_file_cpufreq
-echo -e "\toption governor0 'schedutil'" >> $config_file_cpufreq
-echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
-echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
+config_file_cpufreq="$(find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq' | head -n 1)"
+
+if [ -n "$config_file_cpufreq" ] && [ -f "$config_file_cpufreq" ]; then
+  truncate -s-1 "$config_file_cpufreq"
+  printf "\toption governor0 'schedutil'\n" >> "$config_file_cpufreq"
+  printf "\toption minfreq0 '816000'\n" >> "$config_file_cpufreq"
+  printf "\toption maxfreq0 '1512000'\n\n" >> "$config_file_cpufreq"
+fi
 
 # luci-app-jd-dailybonus
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
