c2d062fc9f48d59f1d4368407813c57eff822397
Nonosword <37948612+Nonosword@users.noreply.github.com>
2026-03-29 00:58:50 +0800

    Update patches_MainNet.sh

--- openwrt/patches_MainNet.sh
+++ openwrt/patches_MainNet.sh
@@ -1,12 +1,4 @@
-#!/usr/bin/env bash
-
-if sed --version >/dev/null 2>&1; then
-  SED_INPLACE=(-i)
-else
-  SED_INPLACE=(-i '')
-fi
-
-sed "${SED_INPLACE[@]}" 's|192.168.1.1|192.168.100.1|' package/base-files/files/bin/config_generate
-sed "${SED_INPLACE[@]}" 's|Openwrt|Devvvvvice_Mainnet|' package/base-files/files/bin/config_generate
-sed "${SED_INPLACE[@]}" 's|OpenWrt|Devvvvvice_Mainnet|' package/base-files/files/bin/config_generate
-sed "${SED_INPLACE[@]}" "s|log_size='64'|log_size='1024'|" package/base-files/files/bin/config_generate
+sed -i 's|192.168.1.1|192.168.100.1|' package/base-files/files/bin/config_generate
+sed -i 's|Openwrt|Devvvvvice_Mainnet|' package/base-files/files/bin/config_generate
+sed -i 's|OpenWrt|Devvvvvice_Mainnet|' package/base-files/files/bin/config_generate
+sed -i "s|log_size='64'|log_size='1024'|" package/base-files/files/bin/config_generate
