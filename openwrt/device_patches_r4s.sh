b8955b4d93cf56d7618e9379a17e52a091b0c206
Nonosword <37948612+Nonosword@users.noreply.github.com>
2026-03-29 10:04:09 +0800

    Update device_patches_r4s.sh

--- openwrt/device_patches_r4s.sh
+++ openwrt/device_patches_r4s.sh
@@ -1,11 +1,11 @@
 sed -i 's|Devvvvvice|R4S|' package/base-files/files/bin/config_generate
 
 # change the voltage value for over-clock stablization
-config_file_cpufreq=`find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq'`
-truncate -s-1 $config_file_cpufreq
-echo -e "\toption governor0 'schedutil'" >> $config_file_cpufreq
-echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
-echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
+# config_file_cpufreq=`find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq'`
+# truncate -s-1 $config_file_cpufreq
+# echo -e "\toption governor0 'schedutil'" >> $config_file_cpufreq
+# echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
+# echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
 
 # luci-app-jd-dailybonus
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
